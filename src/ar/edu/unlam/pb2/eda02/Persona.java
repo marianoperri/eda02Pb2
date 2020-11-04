9b839d99e9c506a7b0c188fc08ccfbe8d2a4b4b3
marianoperri <marianoperri@gmail.com>
2020-11-04 16:36:09 -0300

    cargando las cambios en clase persona

--- src/ar/edu/unlam/pb2/eda02/Persona.java
+++ src/ar/edu/unlam/pb2/eda02/Persona.java
@@ -1,5 +1,30 @@
 package ar.edu.unlam.pb2.eda02;
 
 public abstract class Persona {
+	private String nombre;
+	private String apellido;
+	private Integer edad;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
 
 }
